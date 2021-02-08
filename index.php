c259b105737de3e288b0988ec75797a88f713def
Davide <ciampad06@gmail.com>
2021-02-08 17:15:27 +0100

    Fine esercizio

--- index.php
+++ index.php
@@ -1,49 +1,72 @@
 <?php
     class shop {
+        //attributi
         public $department;
         public $type;
         public $shelf;
         public $stock;
+
+        public function printName(){
+
+            echo "<p>". $this->name."</p>"; 
+
+        }
     }
 
+    //estendo la classe shop, e quindi eredito gli attributi di shop
     class food extends shop{
-        private $name;
+        public $name;
 
-        public function __construct($department, $type, $shelf, $stock){
-
-            $this->name = $name;
-
+        //metodo speciale, che viene chiamato automaticamente quando creo un oggetto 
+        public function __construct($foodName,$department, $type, $shelf, $stock){
+            $this->name = $foodName;
+            $this->department = $department;
+            $this->type = $type;
+            $this->shelf = $shelf;
+            $this->stock = $stock;
         }
         
     }
 
     class drink extends shop{
-        private $name;
-        
-        public function __construct($department, $type, $shelf, $stock ){
+        public $name;
 
-            $this->name = $name;
+        public function __construct($drinkName,$department, $type, $shelf, $stock ){
 
-        }
-    }
-
-    class other extends shop{
-        private $name;
-
-        public function __construct($department, $type, $shelf, $stock ){
-
-            $this->name = $name;
-
+            $this->name = $drinkName;
+            $this->department = $department;
+            $this->type = $type;
+            $this->shelf = $shelf;
+            $this->stock = $stock;
         }
 
     }
 
-    $milk = new food("alimentari", "cibo", 3, 11);
+    class others extends shop{
 
+        public $name;
+
+        public function __construct($elementName,$department, $type, $shelf, $stock){
+
+            $this->name = $elementName;
+            $this->department = $department;
+            $this->type = $type;
+            $this->shelf = $shelf;
+            $this->stock = $stock;
+        }
+
+    }
+
+
+    $bread = new food("pane","alimentari", "cibo", 3, 11);
+    $milk = new drink("latte","alimentari", "cibo", 3, 11);
+    $piatti = new others("piatti", "non alimentari", "servizi", 2 , 50);
+
+    var_dump($bread);
     var_dump($milk);
+    var_dump($piatti);
 
-
-
+    $milk->printName();
 
 
 ?>
